15056741e2d966b9a658bb46d5189b898c303b47
vuahai <vuahai@gmail.com>
2022-10-03 11:01:13 -0700

    second commit

--- clean_passwd/clean_pass.2.py
+++ clean_passwd/clean_pass.2.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 import sys
+import os.path
+
+# Check to see if passwd file exists
+def check_file_exist():
+    if True == os.path.exists(sys.argv[1]):
+        print("File Exists")
+        return True
+    else:
+        return False
 
 # Check for 2 inputs
 def check_args():
     if len(sys.argv) == 3:
         print ("It's TRUE")
-        return 1
+        return True
     else:
-        print ("It's FALSE")
+        return False
 
 # Output of file san user
 def remove_user(passwdfile, user): 
@@ -23,9 +32,12 @@ def new_file(line):
     f.write(line)
 
 def main():
-#    user = sys.argv[2]
-#    passfile = sys.argv[1]
-    if check_args() == 1:
+    if not check_args():
+        print ("Usage: clean_pass.py [passwd_file] [username]")
+    elif not check_file_exist():
+        print("passwd file",sys.argv[1]," doesn't exist, please check.")
+    else:
+        print("2 args and passwd file exists")
         remove_user(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
