2beb0cc3648a26adf9c1f91c6cd2bd43c7e05abe
vuahai <vuahai@gmail.com>
2022-10-05 21:39:52 -0700

    update features

--- clean_passwd/parseArg.py
+++ clean_passwd/parseArg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import deluser
 
 server = []
 user = []
@@ -10,13 +11,31 @@ def read_args():
     print("Number of arguments are:",args_length)
     x = 0
     while x < args_length - 1:
-        s =  sys.argv[x+1].split(":")[0]
+        s = sys.argv[x+1].split(":")[0]
         u = sys.argv[x+1].split(":")[1]
-        print("server is:",s,"user is:",u)
+#        print("server is:", s, "user is:", u)
         x = x + 1
         server.append(s)
         user.append(u)
 
-read_args()
 
-print(server[2],user[2])
+def main():
+    read_args()
+    y = 0
+    while y < len(server):
+        if not deluser.check_file_exist(server[y]):
+            print("passwd file", server[y], " doesn't exist, please check.")
+        elif not deluser.check_user(server[y], user[y]):
+            print("User", user[y], "does not exists in", server[y])
+        else:
+            print("checking out file", server[y])
+            deluser.checkout(server[y])
+            print("Removing", user[y], "from", server[y])
+            deluser.remove_user(server[y], user[y])
+            print("checking in file", server[y])
+            deluser.checkin(user[y], server[y])
+        y = y + 1
+
+
+if __name__ == "__main__":
+    main()
