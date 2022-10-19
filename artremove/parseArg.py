8d21384d67f78a8359ab195969330b09a79309ec
vuahai <vuahai@gmail.com>
2022-10-19 13:11:54 -0700

    adding ceph notes

---
+++ artremove/parseArg.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys
+import deluser
+
+server = []
+user = []
+
+def read_args():
+    args_length = len(sys.argv)
+    print("Number of arguments are:",args_length)
+    x = 0
+    while x < args_length - 1:
+        s = sys.argv[x+1].split(":")[0]
+        u = sys.argv[x+1].split(":")[1]
+#        print("server is:", s, "user is:", u)
+        x = x + 1
+        server.append(s)
+        user.append(u)
+
+
+def main():
+    read_args()
+    y = 0
+    while y < len(server):
+        if not deluser.check_file_exist(server[y]):
+            print("passwd file", server[y], " doesn't exist, please check.")
+        elif not deluser.check_user(server[y], user[y]):
+            print("User", user[y], "does not exists in", server[y])
+        else:
+            print("checking out file", server[y])
+            deluser.checkout(server[y])
+            print("Removing", user[y], "from", server[y])
+            deluser.remove_user(server[y], user[y])
+            print("checking in file", server[y])
+            deluser.checkin(user[y], server[y])
+        y = y + 1
+
+
+if __name__ == "__main__":
+    main()
